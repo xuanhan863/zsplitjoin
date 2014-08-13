a676e06b9c24fc32d17a335610aaff05eda3c2da
Marcelo Tambalo <marcelo.zokis@gmail.com>
2014-08-13 13:56:39 -0300

    add OptionParser to join

--- zjoin.py
+++ zjoin.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python
 import sys
+from optparse import OptionParser
 from zsplitjoin import join_file
 
 
 def main():
-    join_file(sys.argv[1], exit_file_name=sys.argv[2])
+    opar = OptionParser()
+    opar.add_option("-o", "--output", dest="output",
+                    help="output file name", type="str", default=None)
+    opar.add_option("-c", "--chunk", dest="chunk",
+                    help="chunk size", type="str", default='1K')
+
+    options, args = opar.parse_args(sys.argv)
+    if len(args) <= 1:
+        print 'invalid input'
+        sys.exit(0)
+
+    join_file(args[1], output_file_name=options.output, chunk_size=options.chunk)
 
 
 if __name__ == '__main__':
--- zsplitjoin/join_files.py
+++ zsplitjoin/join_files.py
@@ -1,18 +1,19 @@
 # coding: utf-8
 from unipath import Path
-from .utils import create_file_name, files_iter, read_in_chunks
+from .utils import create_file_name, files_iter, get_size, read_in_chunks
 
 
-def join_file(path, exit_file_name=None, chunk_size=1024):
+def join_file(path, output_file_name=None, chunk_size='1K'):
+    chunk_size = get_size(chunk_size)
     path = Path(path)
-    if exit_file_name is None:
-        exit_file_path = create_file_name(path)
+    if output_file_name is None:
+        output_file_path = create_file_name(path)
     else:
-        exit_file_path = Path(exit_file_name)
+        output_file_path = Path(output_file_name)
 
-    exit_file_path.write_file('', 'wb')
+    output_file_path.write_file('', 'wb')
 
-    with open(exit_file_path, 'wb') as exit_file:
+    with open(output_file_path, 'wb') as exit_file:
         for cur_file_name in files_iter(path):
             with open(cur_file_name, 'rb') as cur_file:
                 for piece in read_in_chunks(cur_file, chunk_size):
