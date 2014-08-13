db95faa20b8aae2a0f247d56b96306350d738db5
Marcelo Tambalo <marcelo.zokis@gmail.com>
2014-08-13 13:57:23 -0300

    add OptionParser to slip and fix bug of option size

--- zsplit.py
+++ zsplit.py
@@ -1,10 +1,32 @@
 #!/usr/bin/env python
 import sys
+from optparse import OptionParser
 from zsplitjoin import split_file
 
 
 def main():
-    split_file(sys.argv[1], number_of_files=int(sys.argv[2]))
+    opar = OptionParser()
+    opar.add_option("-n", "--number", dest="number",
+                    help="number of files", type="int", default=None)
+    opar.add_option("-s", "--size", dest="size",
+                    help="size of the output files", type="str", default=None)
+    opar.add_option("-o", "--output", dest="output",
+                    help="output file name", type="str", default=None)
+    opar.add_option("-c", "--chunk", dest="chunk",
+                    help="chunk size", type="str", default='1K')
+
+    options, args = opar.parse_args(sys.argv)
+    if len(args) <= 1:
+        print 'invalid input'
+        sys.exit(0)
+
+    split_file(
+        args[1],
+        number_of_files=options.number,
+        new_file_size=options.size,
+        output_file_name=options.output,
+        chunk_size=options.chunk
+    )
 
 
 if __name__ == '__main__':
--- zsplitjoin/split_files.py
+++ zsplitjoin/split_files.py
@@ -3,29 +3,33 @@ from unipath import Path
 from .utils import get_size, read_in_chunks
 
 
-def split_file(path, number_of_files=2, new_file_size=None, exit_file_name=None, chunk_size=1024):
-    new_file_size = get_size(new_file_size)
+def split_file(path, number_of_files=2, new_file_size=None, output_file_name=None, chunk_size=1024):
+    if new_file_size:
+        new_file_size = get_size(new_file_size)
     chunk_size = get_size(chunk_size)
-
     path = Path(path)
+
+    origin_file_size = path.size()
+
     if number_of_files is None:
-        number_of_files = path.size() / new_file_size
+        number_of_files = origin_file_size / new_file_size
     else:
-        new_file_size = path.size() / number_of_files
+        new_file_size = origin_file_size / number_of_files
 
-    if exit_file_name is None:
-        exit_file_name = path.name
+    if output_file_name is None:
+        output_file_name = path.name
 
-    new_files_name = '{exit_file_name}.part%d'.format(exit_file_name=exit_file_name)
+    new_files_name = '{output_file_name}.part%d'.format(output_file_name=output_file_name)
     new_files = [new_files_name % i for i in range(1, number_of_files+1)]
 
     with open(path, 'rb') as origin_file:
         file_iter = read_in_chunks(origin_file, chunk_size)
         for cur_file_name in new_files:
+            is_last = cur_file_name == new_files[-1]
             cur_file_path = Path(cur_file_name)
             cur_file_size = 0
             with open(cur_file_path, 'wb') as cur_writing_file:
-                while cur_file_size < new_file_size:
+                while cur_file_size < new_file_size or not is_last:
                     try:
                         piece = next(file_iter)
                     except StopIteration:
