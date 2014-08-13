d7e38bd1666c2e571ce1587a3e6111580975970e
Marcelo Fonseca Tambalo <marcelo.zokis@gmail.com>
2014-08-13 19:52:10 -0300

    Update __init__.py

--- zsplitjoin/__init__.py
+++ zsplitjoin/__init__.py
@@ -2,3 +2,5 @@ from .join_files import join_file
 from .split_files import split_file
 
 __all__ = ['split_file', 'join_file']
+__author__ = "Marcelo Fonseca Tambalo <marcelo.zokis@gmail.com>"
+__version__ = '1.0.0'
