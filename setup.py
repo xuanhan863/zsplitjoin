f287e77df522473c05db10674d18c691757b5b45
Marcelo Tambalo <marcelo.zokis@gmail.com>
2014-08-13 11:01:19 -0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from distutils.core import setup
+
+setup(
+    name='zsplitjoin',
+    version='0.1.0',
+    description='Simple split and join files.',
+    long_description=open('README.md').read(),
+    author='Marcelo Fonseca Tambalo',
+    author_email='marcelo.zokis@gmail.com',
+    url='https://github.com/zokis/zsplitjoin',
+    packages=['zsplitjoin'],
+    classifiers=(
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+    )
+)
